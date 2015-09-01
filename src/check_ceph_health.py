0f55e6f9f6e195fd555e98cc91828569c16d1c60
Jaime Ibar <jim2k7@gmail.com>
2015-09-01 12:27:31 +0100

    Added GPL2 license

--- src/check_ceph_health.py
+++ src/check_ceph_health.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Distributed under GNU/GPL 2 license
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
 #
-#  Copyright (c) 2013 SWITCH http://www.switch.ch
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
 #
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
-#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see http://www.gnu.org/licenses/
+
 
 """
 Ceph nagios plugins
@@ -60,7 +62,7 @@ def _parse_arguments():
 
 def compose_command(arguments):
     """
-	Compose ceph command from command line arguments
+    Compose ceph command from command line arguments
     :param arguments: Command line arguments
     :return: Ceph command or False in case of missing params
     """
